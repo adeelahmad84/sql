a3bf255198d652216a3e7f9c213712d4ab28daa5
Adeel Ahmad <adeelahmad786@gmail.com>
2016-03-10 17:38:21 +0000

    added test modules and docs

--- sql.py
+++ sql.py
@@ -1,7 +1,18 @@
-# Create a SQLite3 database and table
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
 
+'''
+Docstring
+**********
+Title: Create a SQLite3 database and table
+Author: Adeel Ahmad
+Email: adeelahmad84@me.com
 
-# import the sqlite3 library
+'''
+
+__version__ = "0.1"
+
+import unittest
 import sqlite3
 
 # create a new database if the database doesn't already exist
@@ -17,3 +28,11 @@ cursor.execute("""CREATE TABLE population
 
 # close the database connection
 conn.close()
+
+if __name__ == "__main__":
+    app.run()
+    import doctest
+    doctest.testmod()
+    class MyTest(unittest.TestCase):
+        def test(self):
+            self.assertEqual(main(), )
